d818aca2ce00e887a7d7943c78364d32e7e22d98
Vitalij <vitalij.cernysiov@gmail.com>
2022-11-29 13:35:03 +0200

    Start

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      <div className='clock'>
+      <div className='hour'></div>
+      <div className='minute'></div>
+      <div className='number number1'></div>
+      <div className='number number2'></div>
+      <div className='number number3'></div>
+      <div className='number number4'></div>
+      <div className='number number5'></div>
+      <div className='number number6'></div>
+      <div className='number number7'></div>
+      <div className='number number8'></div>
+      <div className='number number9'></div>
+      <div className='number number10'></div>
+      <div className='number number11'></div>
+      <div className='number number12'></div>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
