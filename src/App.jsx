171881bd2fe0f214d077fee0270415b2f6670940
Vitalij <vitalij.cernysiov@gmail.com>
2022-11-29 18:33:37 +0200

    Relation beetween time and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,51 @@
+import { useState } from 'react';
+
 import './App.css';
 
 function App() {
+
+
+  const [hour, setHour] = useState(12);
+  const [minute, setMinute] = useState(0);
+
+  const minutesStyle = {
+    transform: `rotate(${minute * 6}deg)`
+  };
+  const hoursStyle = {
+    transform: `rotate(${hour * 30}deg)`
+  };
+
+  const handleChange = () => {
+    setHour(12);
+    setMinute(0);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <div className='time'>
           <h1 style={{
             marginBottom: '0px'
-          }}>Please, choose your time:</h1>
-        <label>Hours:</label>
-        <input type="number" />
-        <label>Minutes:</label>
-        <input type="number" />
-        <button>Add time</button>
+          }}>Please, select your time:</h1>
+        <label>Hours (between 1 and 12):</label>
+        <input 
+        type="hours"
+        value={hour}
+        onChange={e => setHour(e.target.value)} 
+        />
+        <label>Minutes (between 0 and 59):</label>
+        <input 
+        type="minutes"
+        value={minute}
+        onChange={e => setMinute(e.target.value)}
+         />
+        <button onClick={handleChange}>Drop the time</button>
+        <button>Calculate angle</button>
         </div>
       <div className='clock'>
-      <div className='hour'></div>
+      <div className='hour' style={hoursStyle}></div>
       <div className='center'></div>
-      <div className='minute'></div>
+      <div className='minute' style={minutesStyle}></div>
       <div className='number number1'>1</div>
       <div className='number number2'>2</div>
       <div className='number number3'>3</div>
